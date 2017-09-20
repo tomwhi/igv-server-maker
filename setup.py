591c8b266d06044df752e889fbcd600b8a26d8f7
Thomas Whitington <thomaswhitington@gmail.com>
2017-09-20 10:06:48 +0200

    Setting up project structure.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements("requirements.txt", session=False)
+
+# reqs is a list of requirement
+reqs = [str(ir.req) for ir in install_reqs if ir.req is not None]
+
+setup(
+    name='igvtree',
+    version='0.1.0',
+    packages=find_packages(exclude=('tests*', 'docs', 'examples')),
+    install_requires=reqs,
+    entry_points={
+        'console_scripts': [
+            'igvtree = igvtree.cli:base'
+        ]
+    }
+)
